c3d83ecbda484796c369ab308eb446c527cdbe10
PatrykDurand <pdurand@edu.cdv.pl>
2023-05-27 22:27:03 +0200

    Add trusted recipient - improved recipients handling and resolved errors

--- src/backend/routes/user/add.recipient.ts
+++ src/backend/routes/user/add.recipient.ts
@@ -1,39 +1,45 @@
-import { Request, Response } from 'express'
-import { body } from 'express-validator'
-import { StatusCodes } from 'http-status-codes'
-import { prisma } from '../../database'
-import { TRoute } from '../../routes/types'
-import { handleRequest } from '../../utils/request.utils'
-import { authorize } from '../../utils/middleware.utils'
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { prisma } from '../../database';
+import { TRoute } from '../../routes/types';
+import { handleRequest } from '../../utils/request.utils';
+import { authorize } from '../../utils/middleware.utils';
+import { body } from 'express-validator';
 
 export default {
     method: 'post',
-    path: '/api/recipients/add',
+    path: '/api/recipient/add',
     validators: [
         authorize,
-        body('userID').isNumeric(),
+        body('userId').not().isEmpty(),
         body('name').not().isEmpty(),
         body('surname').not().isEmpty(),
         body('accountNumber').not().isEmpty(),
-        body('trusted').isBoolean(),
     ],
-    handler: async (req: Request, res: Response) =>
-        handleRequest({
-            req,
-            res,
-            responseSuccessStatus: StatusCodes.CREATED,
-            execute: async () => {
-                const { userID, name, surname, accountNumber, trusted } =
-                    req.body
-                return await prisma.recipient.create({
-                    data: {
-                        userID,
-                        name,
-                        surname,
-                        accountNumber,
-                        trusted: Boolean(trusted),
-                    },
-                })
+    handler: async (req: Request, res: Response): Promise<void> => {
+        const { userId, name, surname, accountNumber } = req.body;
+
+        const user = await prisma.user.findUnique({
+            where: { userId },
+        });
+
+        if (!user) {
+            res.status(StatusCodes.NOT_FOUND).json({ error: 'User not found' });
+            return;
+        }
+
+        const recipient = await prisma.recipient.create({
+            data: {
+                name,
+                surname,
+                accountNumber,
+                trusted: true,
+                user: {
+                    connect: { userId },
+                },
             },
-        }),
-} as TRoute
+        });
+
+        res.status(StatusCodes.CREATED).json(recipient);
+    },
+} as TRoute;
